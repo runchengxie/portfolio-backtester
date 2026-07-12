6679290741bf38b85508fc88b9fd7de190fbc723
Richard <709090316@qq.com>
2026-07-12 21:48:16 +0800

    feat: export accounting result contracts

--- src/cstree/backtesting/__init__.py
+++ src/cstree/backtesting/__init__.py
@@ -18,9 +18,17 @@ from .style_replica_portfolio import (
     compute_daily_exposure,
     compute_style_exposure_summary,
 )
+from .turnover import (
+    TurnoverBreakdown,
+    annualize_turnover,
+    name_turnover,
+    turnover_from_trade_weights,
+)
+from .types import CostBreakdown
 
 __all__ = [
     "POSITIONS_BY_REBALANCE_CONTRACT",
+    "CostBreakdown",
     "DetailedTradeFeeModel",
     "GroupCap",
     "PositionBacktestConfig",
@@ -28,6 +36,8 @@ __all__ = [
     "PositionsByRebalanceFrameContract",
     "StrategySpec",
     "StyleReplicaPortfolioConfig",
+    "TurnoverBreakdown",
+    "annualize_turnover",
     "assert_positions_by_rebalance_frame",
     "backtest_topk",
     "build_style_replica_positions",
@@ -36,8 +46,10 @@ __all__ = [
     "compute_style_exposure_summary",
     "construct_positions_from_strategy",
     "l2_price_tiered_slippage",
+    "name_turnover",
     "run_position_backtest",
     "strategy_from_config",
     "summarize_period_returns",
+    "turnover_from_trade_weights",
     "validate_positions_by_rebalance_frame",
 ]
