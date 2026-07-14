13cf43c0eb5045c777d525af2684817086f0a08b
Richard <709090316@qq.com>
2026-07-14 21:41:00 +0800

    feat: expose AFML portfolio APIs

--- src/portfolio_backtester/__init__.py
+++ src/portfolio_backtester/__init__.py
@@ -1,5 +1,13 @@
 from .api import backtest_topk, run_backtest
 from .backtest_spec import BacktestSpec
+from .bet_sizing import (
+    SizingConfig,
+    average_active_bets,
+    build_sized_weights,
+    build_sizing_receipt,
+    discretize_weights,
+    probability_to_size,
+)
 from .contracts import (
     POSITIONS_BY_REBALANCE_CONTRACT,
     GroupCap,
@@ -17,9 +25,18 @@ from .daily_watch20 import (
     select_daily_watch20,
 )
 from .execution import DetailedTradeFeeModel, l2_price_tiered_slippage
+from .hrp import HrpConfig, HrpResult, hierarchical_risk_parity, rolling_hrp_weights
 from .metrics import summarize_period_returns
 from .position_backtest import PositionBacktestConfig, PositionBacktestResult, run_position_backtest
 from .strategy import construct_positions_from_strategy, strategy_from_config
+from .strategy_risk import (
+    StrategyRiskReport,
+    implementation_shortfall_metrics,
+    probabilistic_sharpe_ratio,
+    return_concentration,
+    strategy_failure_probability,
+    summarize_strategy_risk,
+)
 from .turnover import (
     TurnoverBreakdown,
     annualize_turnover,
@@ -39,22 +56,38 @@ __all__ = [
     "DetailedTradeFeeModel",
     "GroupCap",
     "GuardFactorSpec",
+    "HrpConfig",
+    "HrpResult",
     "PositionBacktestConfig",
     "PositionBacktestResult",
     "PositionsByRebalanceFrameContract",
+    "SizingConfig",
+    "StrategyRiskReport",
     "StrategySpec",
     "TurnoverBreakdown",
     "annualize_turnover",
     "assert_positions_by_rebalance_frame",
+    "average_active_bets",
     "backtest_topk",
+    "build_sized_weights",
+    "build_sizing_receipt",
     "construct_positions_from_strategy",
+    "discretize_weights",
+    "hierarchical_risk_parity",
+    "implementation_shortfall_metrics",
     "l2_price_tiered_slippage",
     "name_turnover",
+    "probabilistic_sharpe_ratio",
+    "probability_to_size",
+    "return_concentration",
+    "rolling_hrp_weights",
     "run_backtest",
     "run_position_backtest",
     "select_daily_watch20",
+    "strategy_failure_probability",
     "strategy_from_config",
     "summarize_period_returns",
+    "summarize_strategy_risk",
     "turnover_from_trade_weights",
     "validate_positions_by_rebalance_frame",
 ]
